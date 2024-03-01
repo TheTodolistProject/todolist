d6ba2a4e591bee52942b5dbe0d60b4ca7f9af87e
matin <matinnjt2000@gmail.com>
2024-03-01 12:55:33 +0330

    added redis configurations

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'Redis' => Illuminate\Support\Facades\Redis::class,
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
